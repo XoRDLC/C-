92d058dc59c5f2a1a253e85a766d710047ee7cf5
Dmitriy Klyaus <dmitriy.klyaus@rusvs.ru>
2019-05-15 18:34:12 +0300

    update

--- Project1/Project1/_Main.cs
+++ Project1/Project1/_Main.cs
@@ -7,8 +7,10 @@ namespace Project1
     {
         public static void Main() {
             Log("\n--- start --- [" + DateTime.Now + "]");
+
             Class1 class1 = new Class1();
             class1.Main();
+
             Log("---  end  ---\n");
         }
     }

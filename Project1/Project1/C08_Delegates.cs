5e52c938304cba45b5bea008fe0af23df946ba70
Dimitriy Klyaus <xor@live.ru>
2019-05-19 22:02:18 +0300

    update

---
+++ Project1/Project1/C08_Delegates.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using static Project1.Statics;
+
+namespace Project1
+{
+    class C08_Delegates
+    {
+        public void Main() {
+            Test_NamedDelegates();
+            Test_AnonDelegates();
+        }
+
+
+        #region Named Delegates, Test_NamedDelegates()
+        public delegate void ShowResult(int x);
+        public delegate int GetCasualInt(int x);
+        public delegate int GetRefInt(ref int x);
+
+        public static void Test_NamedDelegates() {
+            int x = 51;
+            int xr = 51;
+
+            ShowResult sr = x < 50 ? new ShowResult( LessThen ) : new ShowResult ( EqualOrMore );
+            sr?.Invoke(x);
+
+            GetCasualInt gci = MultiplyBy2;
+            gci             += MultiplyBy4;
+            GetRefInt gri    = MultiplyBy2;
+            gri             += MultiplyBy4;
+
+            Log("x: " + x + "\txr: " + xr);
+            Log("gci: " + gci?.Invoke(x));
+            Log("gri: " + gri?.Invoke(ref xr));
+
+            gci -= MultiplyBy2;            gci -= MultiplyBy2;
+            gri -= MultiplyBy2;            gri -= MultiplyBy2;
+
+
+            Log("x: " + x + "\txr: " + xr);
+            Log("gci: " + gci?.Invoke(x));
+            Log("gri: " + gri?.Invoke(ref xr));
+
+            gci -= MultiplyBy4;
+            gri -= MultiplyBy4;
+
+            Log("x: " + x + "\txr: " + xr);
+            Log("gci: " + gci?.Invoke(x));
+            Log("gri: " + gri?.Invoke(ref xr));
+        }
+
+        public static void LessThen(int x) {
+            Log(x + " less then 50");
+        }
+
+        public static void EqualOrMore(int x) {
+            if (x == 50) Log(x + " equal 50");
+            else Log(x + " more then 50");
+        }
+
+        public static int MultiplyBy2(int x) { x *= 2; return x; }
+        public static int MultiplyBy4(int x) { x *= 4; return x; }
+        public static int MultiplyBy2(ref int x) { x *= 2; return x; }
+        public static int MultiplyBy4(ref int x) { x *= 4; return x; }
+
+        #endregion
+
+        #region Delegates with Anonymous methods, Test_AnonDelegates()
+        private delegate void AnonD(ref int x);
+        public static void Test_AnonDelegates() {
+            AnonD an = delegate (ref int x)
+            {
+                Log("x: " + x);
+                x *= 3;
+            };
+            an      += delegate (ref int x)
+            {
+                Log("x: " + x);
+            };
+            int y = 4;
+            an?.Invoke(ref y);
+
+            AnonD anX;
+            {
+                //scope of t
+                int t = 4;
+                anX = delegate
+                {
+                    Log("anX, t: " + t);
+                };
+            }
+            //Log(t); not exists
+
+            //t still exists in delegate anX
+            anX.Invoke(ref y);
+
+        }
+        #endregion
+    }
+}
--- Project1/Project1/Class1.cs
+++ Project1/Project1/Class1.cs
@@ -1,33 +1,14 @@
 ﻿using System;
-using static Project1.Statics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Project1
 {
     class Class1
     {
-        public void Main() {
-            //for(int i=0; i<50; i++)
-            Test_Delegates();
-        }
+        public void Main() { }
 
-        delegate void MyDel(int param);
-
-        public static void Test_Delegates() {
-            int rnd = new Random().Next(99);
-            MyDel md = rnd < 50 ? new MyDel(LessThen50) : (rnd > 50 ? new MyDel( GreaterThen50) : new MyDel(Equalent50));
-            md(rnd);
-        }
-
-        public static void LessThen50(int value) {
-            Log(value + " less then 50");
-        }
-
-        public static void GreaterThen50(int value) {
-            Log(value + " greater then 50");
-        }
-
-        public static void Equalent50(int value) {
-            Log("equal 50");
-        }
     }
 }

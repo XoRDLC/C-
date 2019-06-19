249a3eb697e90d9879e7f78bc6be739ddc04d478
Dmitriy Klyaus <dmitriy.klyaus@rusvs.ru>
2019-06-19 16:29:50 +0300

    VBE plugin for CS, init

---
+++ VBEAddInCS/VBEAddInCS/Connect.cs
@@ -0,0 +1,60 @@
+﻿using XoR.Interop;
+using XoR.Interop.Extensibility;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System;
+
+//https://www.mztools.com/articles/2012/MZ2012013.aspx
+namespace VBEAddInCS
+{
+    public class Connect : IDTExtensibility2
+    {
+        private VBIDE.VBE _VBE;
+        private VBIDE.AddIn _AddIn;
+        public void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+        {
+            try
+            {
+                _VBE = (VBIDE.VBE)Application;
+                _AddIn = (VBIDE.AddIn)AddInInst;
+
+                switch (ConnectMode)
+                {
+                    case ext_ConnectMode.ext_cm_Startup:
+                        //OnStartupComplete will be called
+                        break;
+                    case ext_ConnectMode.ext_cm_AfterStartup:
+                        //
+                        InitializeAddIn();
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        public void OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
+        {
+        }
+
+        public void OnAddInsUpdate(ref Array custom)
+        {
+        }
+
+        public void OnStartupComplete(ref Array custom)
+        {
+            InitializeAddIn();
+        }
+
+        public void OnBeginShutdown(ref Array custom)
+        {
+        }
+
+        private void InitializeAddIn()
+        {
+            
+        }
+    }
+}

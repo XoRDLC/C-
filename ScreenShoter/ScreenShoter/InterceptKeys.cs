1286bab138f434a6da977ce228153c9dfff26c8f
XoRDLC <deadlords@mail.ru>
2020-12-20 17:33:47 +0300

    up

--- ScreenShoter/ScreenShoter/InterceptKeys.cs
+++ ScreenShoter/ScreenShoter/InterceptKeys.cs
@@ -28,6 +28,13 @@ namespace ScreenShoter
 
         private FNotification fNotification = null;
 
+
+        /*https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-3.0/ms182351(v=vs.80)?redirectedfrom=MSDN
+         * STAThreadAttribute indicates that the COM threading model for the application is single-threaded apartment. 
+         * This attribute must be present on the entry point of any application that uses Windows Forms; 
+         * if it is omitted, the Windows components might not work correctly. If the attribute is not present, 
+         * the application uses the multithreaded apartment model, which is not supported for Windows Forms.
+         */
         [STAThread]
         public void Run()        {
             _hookID = SetHook(_proc);
@@ -103,21 +110,46 @@ namespace ScreenShoter
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
 
-
+        /* SetWindowsHookEx
+         * https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowshookexw
+         * Installs an application-defined hook procedure into a hook chain. You would install a 
+         * hook procedure to monitor the system for certain types of events. These events are associated 
+         * either with a specific thread or with all threads in the same desktop as the calling thread.
+         */
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr SetWindowsHookEx(int idHook,
             LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
+        /* MarshalAs
+         * https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-3.0/ms172514(v=vs.85)
+         * https://docs.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.marshalasattribute?redirectedfrom=MSDN&view=net-5.0
+         * Indicates how to marshal the data between managed and unmanaged code.
+         * 
+         * UnhookWindowsHookEx
+         * https://docs.microsoft.com/en-us/previous-versions/windows/embedded/dn529146(v=winembedded.70)
+         * Removes a hook procedure installed in a system by the SetWindowsHookEx function.
+         */
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
+
+        /* CallNextHookEx
+         * https://docs.microsoft.com/ru-ru/windows/win32/api/winuser/nf-winuser-callnexthookex?redirectedfrom=MSDN
+         * Passes the hook information to the next hook procedure in the current hook chain. 
+         * A hook procedure can call this function either before or after processing the hook information.         * 
+         */
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
             IntPtr wParam, IntPtr lParam);
-        
+
+        /* GetModuleHandle
+         * https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandlea
+         * Retrieves a module handle for the specified module. The module must have been loaded by the calling process.
+         * To avoid the race conditions described in the Remarks section, use the GetModuleHandleEx function.
+         */
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
     }
+
 }
